f45810d62485546a59c31faace181d131b3125d4
alojine <benaspaliokas@gmail.com>
2024-03-20 14:08:27 +0200

    finito

--- env_handler.lua
+++ env_handler.lua
@@ -3,9 +3,7 @@ local defaults = require("defaults.defaults")
 
 local M = {}
 
--- local target = defaults.default_clean_dir
-local target = "~/Temp"
-local destination = "~/Destination"
+local target = defaults.default_clean_dir
 
 local function is_existing_category(category)
     if not defaults.contains_category(category) then
@@ -26,18 +24,22 @@ local function is_existing_extension(extension)
 end
 
 function M.perform_clean(dir)
-    if dir ~= nil then
-        -- upgrade directory search
-        target = dir
+    if dir == nil then
+        dir = target
     end
 
-    local items = shell_utils.execute_command("ls " .. target)
+    local items = shell_utils.execute_command("ls " .. dir)
     for filename in items:gmatch("[^\r\n]+") do
         local _, _, ext = filename:find("^.+(%..+)$")
         if ext then
             local category = defaults.extract_category(ext)
-            local dir = defaults.get_directory(category)
-            shell_utils.execute_command("mv " .. target .. "/" .. filename .. " " .. dir)
+            if category == nil then
+                print("- Skipped " .. filename)
+            else
+                local target_dir = defaults.get_directory(category)
+                shell_utils.execute_command("mv " .. target .. "/" .. filename .. " " .. target_dir)
+                print("+ Moved " .. filename .. " to " .. target_dir)
+            end
         end
     end
 end
--- main.lua
+++ main.lua
@@ -20,6 +20,19 @@ local function main()
     elseif arg[1] == "-reset" then
         config.reset()
     elseif arg[1] == "-help" then
+        print("Usage: lua main.lua [option] [arguments]")
+        print("Options:")
+        print("  -clean [directory]             Clean up files in the specified directory based on their extensions")
+        print("  -defaults                      Show default settings")
+        print("  -move [extension] [category]   Move files with a specific extension to a different category")
+        print("  -create-cat [category] [directory]  Create a new category with the specified directory")
+        print("  -create-ext [extension] [category]  Create a new extension under the specified category")
+        print("  -remove-cat [category]         Remove the specified category")
+        print("  -remove-ext [extension]        Remove the specified extension from its category")
+        print("  -reset                         Reset configuration settings to default")
+        print("  -help                          Show this help message")
+    else
+        print("Invalid option. Use '-help' for usage instructions.")
     end
 end
 
